64d6a8d6c0f463b23e805e34438cb75dcf1a916d
keomony khun <keomony@gmail.com>
2017-01-23 14:36:42 +0000

    should print the bank statement

--- lib/account.rb
+++ lib/account.rb
@@ -10,12 +10,36 @@ class Account
 
   def deposit(amount, date)
     @balance += amount
-    bank_statement.unshift(["#{date}", "%.2f" % amount, "", "%.2f" % @balance])
+    add_to_bank_statement(date,amount,0,@balance)
   end
 
   def withdraw(amount, date)
     @balance -= amount
-    bank_statement.unshift(["#{date}", "", "%.2f" % amount, "%.2f" % @balance])
+    add_to_bank_statement(date,0,amount,@balance)
   end
+
+  def print_bank_statement
+    print_header_of_bank_statement
+    @bank_statement.each do |statement|
+      puts statement.join("||")
+    end
+  end
+
+  private
+
+  def add_to_bank_statement(date,credit,debit,balance)
+    line_width = 12
+    if credit == 0
+      @bank_statement.unshift([" #{date}".ljust(line_width), "  ".ljust(line_width), " #{'%.2f' % debit}".ljust(line_width), " #{'%.2f' % @balance}".ljust(line_width)])
+    else
+      @bank_statement.unshift([" #{date}".ljust(line_width), " #{'%.2f' % credit}".ljust(line_width), "  ".ljust(line_width), " #{'%.2f' % @balance}".ljust(line_width)])
+    end
+  end
+
+  def print_header_of_bank_statement
+    line_width = 12
+    puts " date".ljust(line_width)+ "|| credit".ljust(line_width+2)+ "|| debit".ljust(line_width+2)+ "|| balance"
+  end
+
 
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,18 +1,18 @@
 require "./lib/account"
 
 describe Account do
-  subject(:mony) { described_class.new("Mony") }
+  subject(:mony_acc) { described_class.new("Mony") }
 
   context "as a new account" do
 
     it {is_expected.to respond_to :name}
 
     it "should have a balance 0" do
-      expect(mony.balance).to eq(0)
+      expect(mony_acc.balance).to eq(0)
     end
 
     it "should have an empty bank statement" do
-      expect(mony.bank_statement).to eq([])
+      expect(mony_acc.bank_statement).to eq([])
     end
 
   end
@@ -20,45 +20,58 @@ describe Account do
   context "as an existing account" do
     describe "#deposit" do
       before do
-        mony.deposit(1000,"10/01/2012")
+        mony_acc.deposit(1000,"10/01/2012")
       end
 
 
       it "should be able to deposit" do
-        expect(mony.balance).to eq(1000)
+        expect(mony_acc.balance).to eq(1000)
       end
 
       it "should add to the bank statement record" do
-        expect(mony.bank_statement).to include(["10/01/2012", "1000.00", "", "1000.00"])
+        expect(mony_acc.bank_statement).to include([" 10/01/2012 ", " 1000.00    ", "            ", " 1000.00    "])
       end
 
       it "should increase the bank statement record by 1" do
-        expect{mony.deposit(1000, "11/01/2012")}.to change{mony.bank_statement.count}.by(1)
+        expect{mony_acc.deposit(1000, "11/01/2012")}.to change{mony_acc.bank_statement.count}.by(1)
       end
 
     end
 
     describe "#withdraw" do
       before do
-        mony.deposit(1000,"10/01/2012")
-        mony.deposit(2000,"13/01/2012")
+        mony_acc.deposit(1000,"10/01/2012")
+        mony_acc.deposit(2000,"13/01/2012")
       end
 
-
       it "should be able to withdraw" do
-        mony.withdraw(500, "14/01/2012")
-        expect(mony.balance).to eq(2500)
+        mony_acc.withdraw(500, "14/01/2012")
+        expect(mony_acc.balance).to eq(2500)
       end
 
       it "should add to the bank statement record" do
-        mony.withdraw(500, "14/01/2012")
-        expect(mony.bank_statement).to include(["14/01/2012", "", "500.00", "2500.00"])
+        mony_acc.withdraw(500, "14/01/2012")
+        expect(mony_acc.bank_statement).to include([" 14/01/2012 ", "            ", " 500.00     ", " 2500.00    "])
       end
 
       it "should increase the bank statement record by 1" do
-        expect{mony.deposit(1000, "11/01/2012")}.to change{mony.bank_statement.count}.by(1)
+        expect{mony_acc.deposit(1000, "11/01/2012")}.to change{mony_acc.bank_statement.count}.by(1)
+      end
+
+    end
+
+    describe "#print_bank_statement" do
+      before do
+        mony_acc.deposit(1000,"10/01/2012")
+        mony_acc.deposit(2000,"13/01/2012")
+        mony_acc.withdraw(500, "14/01/2012")
+      end
+
+      it "should print the bank statement" do
+        expect(mony_acc).to respond_to(:print_bank_statement)
       end
     end
+
   end
 
 end
